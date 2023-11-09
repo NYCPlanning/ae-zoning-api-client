cfcc2836868342c779d03fda7ce2e16db81ff19b
Tyler Matteo <tylermatteo434@gmail.com>
2023-11-09 11:23:06 -0500

    Update project name, remove boilerplate, initial set up prior to installing Kubb dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { externalizeDeps } from "vite-plugin-externalize-deps";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+    externalizeDeps(),
+  ],
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+      fileName: "index",
+    },
+  },
 });
